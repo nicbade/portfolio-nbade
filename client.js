80d3376169c8bd4a7c094221a83d821c2881d287
Nic Bade <chipchapmaniii@gmail.com>
2017-12-18 14:50:58 -0600

    added client.js file

---
+++ client.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+    $('.btn-tooltip').tooltip();
+    $('.label-tooltip').tooltip();
+    $('.pick-class-label').click(function () {
+        var new_class = $(this).attr('new-class');
+        var old_class = $('#display-buttons').attr('data-class');
+        var display_div = $('#display-buttons');
+        if (display_div.length) {
+            var display_buttons = display_div.find('.btn');
+            display_buttons.removeClass(old_class);
+            display_buttons.addClass(new_class);
+            display_div.attr('data-class', new_class);
+        }
+    });
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+    });
+    $("#slider-default").slider({
+        value: 70,
+        orientation: "horizontal",
+        range: "min",
+        animate: true
+    });
+    $('.carousel').carousel({
+        interval: 4000
+    });
+</script>
